22b34a8214882f8e70b3055a7828990ae4c38f6d
LeonBerty29 <leonbertrand29.dev@gmail.com>
2025-11-21 18:04:49 +0100

    handled mobile view for the images modal

--- src/components/home/discover-section.tsx
+++ src/components/home/discover-section.tsx
@@ -15,7 +15,7 @@ const DiscoverSection = async() => {
       {/*  overlay */}
       <div className="absolute inset-0 bg-gradient-to-t from-white via-white/90 to-white/40 pointer-events-none z-10"></div>
 
-      <div className="absolute bottom-20 left-0 right-0 flex items-center justify-center z-20">
+      <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center z-20">
         <div className="text-center px-4 max-w-4xl mx-auto">
           <h1 className="text-3xl md:text-5xl font-light text-primary mb-6 leading-tight">
             {t("discoverTheBest")} <br />
